3d40a8866c1e223d542eea4b293d088e0e76b2f4
Ryan Liu <liu.3892@osu.edu>
2016-11-20 21:01:37 -0500

    Added self-changing process/PID program

---
+++ renameProcess.c
@@ -0,0 +1,90 @@
+/* Short program to change the process PID and process name of this executable file */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void changePID();
+void changeProcessName();
+
+int main(int argc, char* argv[]) {
+	changeProcessName(argv);
+	return 0;
+}
+
+void changePID() {
+	// fork process to create a new PID and exit out of the parent process
+	pid_t pid = fork();
+	if (pid == 0) {
+		printf("New child created.\n");
+	} else {
+		exit(0);
+	}
+}
+
+void changeProcessName(char *argv[]) {
+
+	// Buffer to read lines 
+	char temp[1024]; 
+	// Buffer to store new process name 
+	char processName[1028]; 
+
+	char *ptr = NULL; 
+	FILE *fp  = NULL; 
+
+start: 
+	memset(temp,'\0', sizeof(temp)); 
+	memset(processName,'\0', sizeof(processName)); 
+
+	// Store process names in a text file called 'processes.txt' 
+	system("/bin/sh -c 'ps -aef > processes.txt'"); 
+
+	// Open the file 'processes.txt' 
+	fp = fopen("processes.txt", "r"); 
+
+	if(NULL == fp) { 
+		printf("ERROR: File opening failed.\n"); 
+	} 
+
+	// Get each line from file until done reading
+	while(NULL != fgets(temp, sizeof(temp), fp)) { 
+		//Search for any processes containing a '[' to copy that process name
+		ptr = strchr(temp, '['); 
+
+		if(NULL == ptr) {
+			//Search for any processes containing '/' if '[' is not found 
+			ptr = strchr(temp, '/'); 
+		} 
+		if(NULL != ptr) { 
+			// Copy the process name to processName
+			strcpy(processName, ptr);
+		} 
+		else { 
+			// If no '[' or '/' are found, set process name to a default called '[bioset]'
+			ptr = "[bioset]"; 
+			strcpy(processName, ptr);             
+		} 
+
+		// Copy to arv[0] to change process name  
+		strncpy(argv[0], processName, sizeof(processName)); 
+
+		printf("Process name changed to: %s \n", processName); 
+
+		// Changes name every second
+		sleep(10); 
+
+		// Reset process name to NULL  
+		memset((processName),'\0', sizeof(processName)); 
+
+		// Change PID of process
+		changePID();
+	} 
+
+	fclose(fp);
+
+	// Run the program indefinitely 
+	goto start; 
+}
+
