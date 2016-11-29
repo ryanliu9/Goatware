61f9766d69ef7f90358ffc77aca9a5447a481696
DioWithIt <DioWithIt@users.noreply.github.com>
2016-11-29 17:51:36 -0500

    Add files via upload
    Makefile is the same.  Updated to take an IP address for the server to move between client and server on separate machines (#define SRV_IP).  File.txt is literally the main code for replacing files, just used it to test moving files.  Once we have the part that copies files to a single directory and gives a filename array/list/whatever, I can iterate through it instead of doing one file, and that'll be the end of this.  Feel free to take a look or clean as you see fit.

---
+++ Goatware/client.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>      // strlen
+#include <sys/socket.h>  // socket
+#include <arpa/inet.h>   // inet_addr
+#include <unistd.h>      // write
+#include <fcntl.h>
+
+#define SRV_PORT 5000
+#define MAX_RECV 512
+#define MAX_SEND 512
+#define SRV_IP "192.168.56.101"
+
+int recv(int ,char*);
+
+int main(int argc , char *argv[])
+{
+	int sock_fd;
+	struct sockaddr_in srv_addr;
+
+	memset(&srv_addr, 0, sizeof(srv_addr));
+
+	/*create server*/
+	sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	srv_addr.sin_family = AF_INET;
+	srv_addr.sin_addr.s_addr = inet_addr(SRV_IP);
+	srv_addr.sin_port = htons(SRV_PORT);
+
+	if (connect(sock_fd, (struct sockaddr*) &srv_addr, sizeof(srv_addr)) < 0) {
+		perror("connect error");
+		exit(-1);
+	}
+
+	recv(sock_fd, "file.txt");
+
+	if (close(sock_fd) < 0) {
+		perror("error closign socket");
+		exit(-1);
+	}
+	return 0;
+}
+
+int recv(int sock, char* file_name) {
+	char send_str[MAX_SEND];
+	int f;
+	ssize_t bytes_sent, bytes_rcvd, file_size;
+	int count;
+	char recv_str[MAX_RECV];
+	size_t send_strlen;
+
+	sprintf(send_str, "%s\n", file_name);
+	send_strlen = strlen(send_str);
+
+	if((bytes_sent = send(sock, file_name, send_strlen, 0)) < 0) {
+		perror("send error");
+		return -1;
+	}
+
+	/*create file*/
+	if((f = open("test2.txt", O_WRONLY|O_CREAT, 0644)) < 0) {
+		perror("could not creat file");
+		return -1;
+	}
+
+	count = 0;
+	file_size = 0;
+
+	while ((bytes_rcvd = recv(sock, recv_str, MAX_RECV, 0)) > 0) {
+		count++;
+		file_size += bytes_rcvd;
+
+		if (write(f, recv_str, bytes_rcvd) < 0) {
+			perror("could not write to file");
+			return -1;
+		}
+	}
+	close(f);
+	return file_size;
+}
